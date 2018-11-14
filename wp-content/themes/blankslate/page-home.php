2c242ecc1b2287553dff4b177377cbcf6aede315
Nataly <ncgarzon88@gmail.com>
2018-11-14 18:41:45 -0500

    Initial commit

---
+++ wp-content/themes/blankslate/page-home.php
@@ -0,0 +1,14 @@
+<?php
+
+// Template Name: Home Page
+
+?>
+
+<h1> <?php the_title(); ?></h1>
+
+<p>
+    <?php the_field('intro_blurb'); ?>
+    <!-- the field is somthing that is part of our plug in. Then we are adding the slug -->
+</p>
+
+<img src="<?php the_field('hero_image')?>" alt="home_image">
